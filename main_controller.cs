ce2ea3cf0e409b1e44c8d6d2e4a415d9f825b606
Forest <forest-jiang@users.noreply.github.com>
2016-02-29 18:36:32 -0800

    Update main_controller.cs
    Latest C# code for sound generator

--- main_controller.cs
+++ main_controller.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,35 +9,24 @@ public class main_controller : MonoBehaviour {
 
     // Audio sources
     string absolutePath = ".\\"; // relative path to where the app is running
-    AudioSource src;
-    List<AudioClip> clips = new List<AudioClip>();
+    List<AudioClip> clips;
     //int numClipsLastPlayed;
     //random number 
     float randomDelayLow, randomDelayHigh;
-
-
-    // All sources
-    private List<GameObject> sources;
-    public GameObject source0;
-    public GameObject source1;
-    public GameObject source2;
-    public GameObject source3;
-    public GameObject source4;
-    public GameObject source5;
-    public GameObject source6;
-    public GameObject source7;
-    public GameObject source8;
-    public GameObject source9;
+    int NUM_OF_BALLS = 9;
+    private List<GameObject> balls;
+    private GameObject objprefab;
     
     // Text box
     public Text dispText;
 
     // File IO related
     FileSystemWatcher watcher;
-    string locSoundFile = "location_sound.txt";
+    string locSoundFile;
     string configFile = "config.txt";
     bool useKeyboard;
     bool enableTextDisp;
+    bool flagFileChanged;   // for file watcher
     // Use this for initialization
     void Start () {
         parseConfigFile();
@@ -45,30 +34,24 @@ public class main_controller : MonoBehaviour {
         absolutePath = Path.GetFullPath(absolutePath);
         // Prepare the audio files
         reloadSounds();
-
-        // Set up the list of object
-        sources = new List<GameObject>();
-        sources.Add(source0);
-        sources.Add(source1);
-        sources.Add(source2);
-        sources.Add(source3);
-        sources.Add(source4);
-        sources.Add(source5);
-        sources.Add(source6);
-        sources.Add(source7);
-        sources.Add(source8);
-        sources.Add(source9);
-
+        clips = new List<AudioClip>();
+        
+        objprefab = (GameObject) Resources.Load("Lightbulbprefab");
+        balls = new List<GameObject>();
+        for (int j = 0; j<NUM_OF_BALLS; j++) balls.Add(Instantiate(objprefab));
         // File watcher
+        locSoundFile = "location_sound.txt"; // the file initially watch
         watcher = new FileSystemWatcher();
         watcher.Path = ".";
         // Watch for change of LastWrite
         watcher.NotifyFilter = NotifyFilters.LastWrite;
-        // Only watch one file
-        watcher.Filter = locSoundFile;
+        // watch all similar files
+        watcher.Filter = "location_sound*.txt";
         // Add event handlers.
         watcher.Changed += new FileSystemEventHandler(OnChanged);
+        watcher.Created += new FileSystemEventHandler(OnChanged);
         watcher.EnableRaisingEvents = true;
+        flagFileChanged = false;
 
     }
 
@@ -90,38 +73,47 @@ public class main_controller : MonoBehaviour {
     private void OnChanged(object source, FileSystemEventArgs e)
     {
         // Specify what is done when a file is changed, created, or deleted.
-        //dispText.text += e.FullPath[0];
-        if (!useKeyboard) readFileMoveAndPlay();
-        
-
+        if (!useKeyboard) {
+            flagFileChanged = true;
+            locSoundFile = e.Name;
+        }
     }
     // Update is called once per frame
     void FixedUpdate () {
         if (useKeyboard && Input.GetKeyDown(KeyCode.Space))
         {
-            readFileMoveAndPlay();
-            if (enableTextDisp) dispText.text += clips[0].name;
+            readFileMoveAndPlay(locSoundFile);
+        }
+
+        if (!useKeyboard && flagFileChanged) {
+            readFileMoveAndPlay(locSoundFile);
+            flagFileChanged = false;
         }
     }
 
-    void readFileMoveAndPlay()
+    void readFileMoveAndPlay(string name)
     {
-        StreamReader reader = File.OpenText(locSoundFile);
+        //Debug.Log("Start loading " + name);
+        StreamReader reader = File.OpenText(name);
         string line;
         int i = 0;
+		
         while ((line = reader.ReadLine()) != null)
         {
+            if (enableTextDisp) dispText.text += "l:" + line + ":l";
             string[] items = line.Split(',');
             float x = float.Parse(items[0]);
             float y = float.Parse(items[1]);
             float z = float.Parse(items[2]);
             int c = int.Parse(items[3]);
-            sources[i].transform.position = new Vector3(x, y, z);
-            i++;
+
+            balls[i].transform.position = new Vector3(x, y, z);
             if (enableTextDisp)dispText.text += "S"+c.ToString();
-            AudioSource asource = sources[i].GetComponent<AudioSource>();
+            AudioSource asource = balls[i].GetComponent<AudioSource>();
             asource.clip = clips[c];
             asource.PlayDelayed(Random.Range(randomDelayLow,randomDelayHigh));
+
+            i++;
 
         }
         reader.Close();
@@ -143,7 +135,7 @@ public class main_controller : MonoBehaviour {
             // debug: dispText.text += f.FullName;
             if (validFileType(f.FullName))
             {
-                Debug.Log("Start loading "+f.FullName);
+                //Debug.Log("Start loading "+f.FullName);
                 StartCoroutine(loadFile(f.FullName));
             }
         }
